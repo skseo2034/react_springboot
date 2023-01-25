e5a688bba8a8edbb6ed4b036a5b4efa3fc45afda
서수교 <sk.seo@navercorp.com>
2023-01-25 10:15:05 +0900

    2023-01-25 : 학습

--- book/book-backend/src/main/java/com/cos/book/web/BookController.java
+++ book/book-backend/src/main/java/com/cos/book/web/BookController.java
@@ -18,6 +18,8 @@ public class BookController {
         return new ResponseEntity<String>("ok", HttpStatus.OK);
     }
 
+    // security (라이브러리 적용) - CORS 정책을 가지고 있음.
+    // @CrossOrigin 은 의미가 없음. 나중에 security 에서  cors 해제 처리를 해야 함.
     @CrossOrigin
     @PostMapping("/book")
     public ResponseEntity<?> save(@RequestBody Book book) {
@@ -40,16 +42,18 @@ public class BookController {
         return new ResponseEntity<>(bookService.모두가져오기(), HttpStatus.OK);
     }
 
+    @CrossOrigin
     @GetMapping("/book/{id}")
     public ResponseEntity<?> findById(@PathVariable Long id) {
         return new ResponseEntity<>(bookService.한건가져오기(id), HttpStatus.OK);
     }
 
+    @CrossOrigin
     @PutMapping("/book/{id}")
     public ResponseEntity<?> update(@PathVariable Long id, @RequestBody Book book) {
         return new ResponseEntity<>(bookService.수정하기(id, book), HttpStatus.OK);
     }
-
+    @CrossOrigin
     @DeleteMapping("/book/{id}")
     public ResponseEntity<?> deleteById(@PathVariable Long id) {
         return new ResponseEntity<>(bookService.삭제하기(id), HttpStatus.OK);
--- book/book-frontend/src/pages/book/Detail.jsx
+++ book/book-frontend/src/pages/book/Detail.jsx
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 const Detail = () => {
+	const { id } = useParams();
+	const [book, setBook] = useState({
+		id: '',
+		title: '',
+		author: '',
+	});
+
+	const getBook = async () => {
+		const response = await fetch('http://localhost:8080/book/' + id).then(res => res.json());
+		return response;
+	};
+
+	useEffect(() => {
+		getBook().then(res => {
+			console.log('aaa', res);
+			setBook(res);
+		});
+	}, []);
+
 	return (
 		<div>
 			<h1>책 상세보기</h1>
+
+			<hr />
+			<h3>{book.author}</h3>
+			<h1>{book.title}</h1>
 		</div>
 	);
 };
