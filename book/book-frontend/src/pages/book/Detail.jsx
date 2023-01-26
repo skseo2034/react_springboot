f9b7a2b4f1051c8d120be354514e2c166a7c837a
서수교 <sk.seo@navercorp.com>
2023-01-26 10:49:56 +0900

    2023-01-26 : 학습

--- book/book-frontend/src/pages/book/Detail.jsx
+++ book/book-frontend/src/pages/book/Detail.jsx
@@ -1,7 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
+import { Button } from 'react-bootstrap';
+import { nativeTouchData } from 'react-dom/test-utils';
 
 const Detail = () => {
+	const navigate = useNavigate();
 	const { id } = useParams();
 	const [book, setBook] = useState({
 		id: '',
@@ -21,10 +24,35 @@ const Detail = () => {
 		});
 	}, []);
 
+	/*const deleteBook = id => {*/
+	const deleteBook = () => {
+		fetch('http://localhost:8080/book/' + id, {
+			method: 'DELETE',
+		})
+			.then(res => res.text())
+			.then(res => {
+				if (res === 'ok') {
+					navigate('/');
+				} else {
+					alert('삭제실패!!!');
+				}
+			});
+	};
+
+	const updateBook = () => {
+		navigate('/updateForm/' + id);
+	};
+
 	return (
 		<div>
 			<h1>책 상세보기</h1>
-
+			<Button variant="warning" onClick={updateBook}>
+				수정
+			</Button>{' '}
+			{/*<Button variant="danger" onClick={() => deleteBook(book.id)}>*/}
+			<Button variant="danger" onClick={deleteBook}>
+				삭제
+			</Button>
 			<hr />
 			<h3>{book.author}</h3>
 			<h1>{book.title}</h1>
--- book/book-frontend/src/pages/book/UpdateForm.jsx
+++ book/book-frontend/src/pages/book/UpdateForm.jsx
@@ -1,10 +1,97 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Button, Form } from 'react-bootstrap';
+import { useNavigate, useParams } from 'react-router-dom';
 
 const UpdateForm = () => {
+	const navigate = useNavigate();
+	const { id } = useParams();
+	const [book, setBook] = useState({
+		title: '',
+		author: '',
+	});
+
+	const getBook = async () => {
+		const response = await fetch('http://localhost:8080/book/' + id).then(res => res.json());
+		return response;
+	};
+
+	useEffect(() => {
+		getBook().then(res => {
+			console.log('aaa', res);
+			setBook(res);
+		});
+	}, []);
+
+	const changeValue = e => {
+		setBook({
+			...book,
+			[e.target.name]: e.target.value,
+		});
+	};
+
+	const submitBook = e => {
+		e.preventDefault(); // submit 이 action을 안타고 자기 할일을 그만함.
+		fetch('http://localhost:8080/book/' + id, {
+			method: 'PUT',
+			headers: {
+				'Content-Type': 'application/json;charset=utf-8',
+			},
+			body: JSON.stringify(book),
+		})
+			.then(res => {
+				console.log(1, res);
+				if (res.status === 200) {
+					return res.json();
+				} else {
+					return null;
+				}
+			})
+			.then(res => {
+				console.log(2, res);
+				if (res !== null) {
+					console.log(3, navigate);
+					navigate('/book/' + id);
+				} else {
+					alert('책 수정에 실패 하였습니다.');
+				}
+			});
+		/*.then(res => {
+				navigate('/');
+			})
+			.catch(error => {
+				console.log('save book error', error);
+				alert('책 등록에 실패 하였습니다.');
+			})*/
+	};
+
 	return (
-		<div>
-			<h1>책 수정하기</h1>
-		</div>
+		<Form onSubmit={submitBook}>
+			<Form.Group className="mb-3" controlId="formBasicEmail">
+				<Form.Label>Title</Form.Label>
+				<Form.Control
+					type="text"
+					placeholder="Enter Title"
+					onChange={changeValue}
+					name="title"
+					value={book.title}
+				/>
+			</Form.Group>
+
+			<Form.Group className="mb-3" controlId="formBasicEmail">
+				<Form.Label>Author</Form.Label>
+				<Form.Control
+					type="text"
+					placeholder="Enter Author"
+					onChange={changeValue}
+					name="author"
+					value={book.author}
+				/>
+			</Form.Group>
+
+			<Button variant="primary" type="submit">
+				Submit
+			</Button>
+		</Form>
 	);
 };
 
